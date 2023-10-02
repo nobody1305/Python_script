026072f1abf8a3a78e530b10d5319ef705de3726
fifa0903 <mf13052001@gmail.com>
2023-10-02 15:19:42 +0700

    python script

--- list.py
+++ list.py
@@ -2,3 +2,6 @@ my_list =["apple", "banana", "stroberry"]
 print(my_list[2])
 my_list.append("mango")
 print(my_list[3])
+
+my_list.remove("apple")
+print(my_list)
--- set.py
+++ set.py
@@ -1,3 +1,7 @@
 my_set = {"january", "february", "march"}
 for element in my_set:
     print(element)
+my_set.add("april")
+print(my_set)
+my_set.remove("january")
+print(my_set)
