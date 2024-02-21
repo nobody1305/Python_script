24847c15fad641cc3722c78b4bb87e01ebf43e4b
fifa0903 <mf13052001@gmail.com>
2024-02-21 15:15:20 +0700

    new

--- machine_learning/beginner.py
+++ machine_learning/beginner.py
@@ -1,7 +1,18 @@
 # import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 df = pd.read_csv('machine_learning/magic04.data', names=cols)
 df.head()
-print(df)
+print(df.head())
+df["class"] = (df["class"] == "g").astype(int)
+print(df["class"])
+
+for label in cols[:-1]:
+  plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+  plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+  plt.title(label)
+  plt.ylabel("Probability")
+  plt.xlabel(label)
+  plt.legend()
+  plt.show()
